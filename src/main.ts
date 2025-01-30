fa8931831a779e58382478f6324ac88912057652
GarryLed <s00256356@atu.ie>
2025-01-30 22:09:04 +0000

    testing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/app.component'; // tell angular to render the AppComponent at the top 
 
+// generates the angular application using the Appcomponent 
+// 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
